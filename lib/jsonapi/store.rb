24c6cbbd1144fb0be1399e86e4d1ec98701d2249
Alex Semyonov <alex@semyonov.us>
2016-10-05 02:08:00 +0300

    Describe interface of JSONAPI::Store instance

--- lib/jsonapi/store.rb
+++ lib/jsonapi/store.rb
@@ -6,5 +6,40 @@ module JSONAPI
   class Store
     autoload :Entity, 'jsonapi/store/entity'
     autoload :Types, 'jsonapi/store/types'
+
+    # @param [JSONAPI::Store::Entity] entity
+    # @return [JSONAPI::Store] store itself
+    def <<(entity)
+      raise NotImplementedError
+    end
+
+    # @param [String] identifier
+    # @return [JSONAPI::Store::Entity]
+    def [](identifier)
+      raise NotImplementedError
+    end
+
+    # @return [<String>]
+    def types
+      raise NotImplementedError
+    end
+
+    # @return [Integer]
+    def size
+      raise NotImplementedError
+    end
+
+    # @return [<JSONAPI::Store::Entity>]
+    def entities
+      raise NotImplementedError
+    end
+
+    # @overload each(&block)
+    #   @return [<JSONAPI::Store::Entity>]
+    # @overload each
+    #   @return [Enumerator<JSONAPI::Store::Entity>]
+    def each
+      raise NotImplementedError
+    end
   end
 end
--- spec/jsonapi/store_spec.rb
+++ spec/jsonapi/store_spec.rb
@@ -2,4 +2,17 @@
 require 'spec_helper'
 
 RSpec.describe JSONAPI::Store do
+  describe 'class' do
+  end
+
+  describe 'instance' do
+    subject(:store) { described_class.new }
+
+    it { is_expected.to respond_to :<< }
+    it { is_expected.to respond_to :size }
+    it { is_expected.to respond_to :[] }
+    it { is_expected.to respond_to :types }
+    it { is_expected.to respond_to :entities }
+    it { is_expected.to respond_to :each }
+  end
 end
